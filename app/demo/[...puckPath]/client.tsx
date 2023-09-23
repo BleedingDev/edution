1b3d95c80f4d62bf122327720a00f76fe4a1444d
Petr Glaser <syreanis+1@gmail.com>
2023-09-23 03:38:28 +0200

    fix(build): don't return children as object

--- app/demo/[...puckPath]/client.tsx
+++ app/demo/[...puckPath]/client.tsx
@@ -1,28 +1,36 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import type { Data } from "@measured/puck"
-import { Button, Puck, Render } from "@measured/puck"
-import headingAnalyzer from "@measured/puck-plugin-heading-analyzer"
+import { useState } from "react";
+import type { Data } from "@measured/puck";
+import { Button, Puck, Render } from "@measured/puck";
+import headingAnalyzer from "@measured/puck-plugin-heading-analyzer";
 
-import config from "../../../puck.config"
+import config from "../../../puck.config";
 
-const isBrowser = typeof window !== "undefined"
+const isBrowser = typeof window !== "undefined";
 
-export function Client({ path, dataBE, isEdit }: { path: string; dataBE: Data; isEdit: boolean }) {
-  const key = `edution-demo:${path}`
+export function Client({
+  path,
+  dataBE,
+  isEdit,
+}: {
+  path: string;
+  dataBE: Data;
+  isEdit: boolean;
+}) {
+  const key = `edution-demo:${path}`;
 
   const [data] = useState<Data>(() => {
     if (isBrowser) {
-      const dataStr = localStorage.getItem(key)
+      const dataStr = localStorage.getItem(key);
 
       if (dataStr) {
-        return JSON.parse(dataStr)
+        return JSON.parse(dataStr);
       }
 
-      return undefined
+      return undefined;
     }
-  })
+  });
 
   if (isEdit) {
     return (
@@ -30,19 +38,19 @@ export function Client({ path, dataBE, isEdit }: { path: string; dataBE: Data; i
         config={config as any}
         data={data}
         onPublish={async (data: Data) => {
-          localStorage.setItem(key, JSON.stringify(data))
+          localStorage.setItem(key, JSON.stringify(data));
         }}
         plugins={[headingAnalyzer]}
         renderHeaderActions={() => (
           <>
-            <Button href={`/demo${path}`} newTab variant='secondary'>
+            <Button href={`/demo${path}`} newTab variant="secondary">
               View page
             </Button>
           </>
         )}
       />
-    )
+    );
   }
 
-  return <Render config={config as any} data={data} />
+  return <Render config={config as any} data={data} />;
 }
