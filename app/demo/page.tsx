c8836dbd3018c805585feb0d31dd486c95599c40
Petr Glaser <syreanis+1@gmail.com>
2023-09-23 03:08:14 +0200

    fix(build): remove fs shit

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,1 +1,47 @@
-export { default, generateMetadata } from "./[...puckPath]/page"
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { Data } from "@measured/puck";
+
+import { Client } from "./[...puckPath]/client";
+import resolvePuckPath from "./[...puckPath]/resolve-puck-path";
+
+// Replace with call to your database
+const getPage = (path: string) => {
+  const allData: Record<string, Data> | null = null;
+
+  return allData ? allData[path] : null;
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { puckPath: string[] };
+}): Promise<Metadata> {
+  const { isEdit, path } = resolvePuckPath(params.puckPath);
+
+  if (isEdit) {
+    return {
+      title: "Puck: " + path,
+    };
+  }
+
+  return {
+    title: getPage(path)?.root?.title,
+  };
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { puckPath: string[] };
+}) {
+  const { isEdit, path } = resolvePuckPath(params.puckPath);
+
+  const data = getPage(path);
+
+  if (!data && !isEdit) {
+    return notFound();
+  }
+
+  return <Client isEdit={isEdit} path={path} dataBE={data} />;
+}
