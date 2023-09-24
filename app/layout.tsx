aed661b92c935c9946f62e005fd868d9690e6d16
Petr Glaser <syreanis+1@gmail.com>
2023-09-24 18:31:40 +0200

    feat(analytics): add partytown with ga4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Lexend } from "next/font/google";
+import { Partytown } from "@builder.io/partytown/react";
+import Head from "next/head";
 
 import "./styles.css";
 
@@ -12,9 +14,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={font.className}>
+      <Head>
+        <Partytown debug={true} forward={["gtag", "dataLayer.push"]} />
+      </Head>
       <body className="overflow-x-hidden">
         {children}
         <Analytics />
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script
+          type="text/partytown"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_KEY}`}
+        />
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            window.gtag = function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA4_KEY}', {
+                page_path: window.location.pathname,
+            });
+        `,
+          }}
+        />
       </body>
     </html>
   );
--- next.config.js
+++ next.config.js
@@ -13,4 +13,7 @@ module.exports = withBuilderDevTools({
       },
     ],
   },
+  experimental: {
+    nextScriptWorkers: true,
+  },
 });
