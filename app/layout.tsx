93baec74b692db8d14fb75a2fadc118f4510ca46
Petr Glaser <syreanis+1@gmail.com>
2023-09-24 20:38:59 +0200

    feat(tracking): add FB pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Lexend } from "next/font/google";
 import { Partytown } from "@builder.io/partytown/react";
+import { Tracking } from "components/Tracking";
 
 import "./styles.css";
 
@@ -17,25 +18,7 @@ export default function RootLayout({
         <Partytown debug={true} forward={["gtag", "dataLayer.push"]} />
         {children}
         <Analytics />
-        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script
-          type="text/partytown"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_KEY}`}
-        />
-        <script
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            window.gtag = function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.NEXT_PUBLIC_GA4_KEY}', {
-                page_path: window.location.pathname,
-            });
-        `,
-          }}
-        />
+        <Tracking />
       </body>
     </html>
   );
