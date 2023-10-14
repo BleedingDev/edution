18bcc219cb1d34241aabaa1d443aed14e99c7af9
Petr Glaser <syreanis+1@gmail.com>
2023-10-15 01:11:32 +0200

    style(prettier): use prettier formatting

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -3,7 +3,10 @@ import { AskLLM } from "components/AskLLM";
 export default async function Page() {
   return (
     <>
-      <AskLLM predefinedPrompt="Generate a comma-separated list of 10 ice cream flavours:" predefinedOutput="Chocolate, Vanilla, Strawberry, Mint Chip, Rocky Road, Cookie Dough, Butter Pecan, Neapolitan, Coffee, Coconut" />
+      <AskLLM
+        predefinedPrompt="Generate a comma-separated list of 10 ice cream flavours:"
+        predefinedOutput="Chocolate, Vanilla, Strawberry, Mint Chip, Rocky Road, Cookie Dough, Butter Pecan, Neapolitan, Coffee, Coconut"
+      />
     </>
   );
 }
