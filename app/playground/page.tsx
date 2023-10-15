9d9d62171b120edd721043df31639bc79e4dea5f
Petr Glaser <syreanis+1@gmail.com>
2023-10-15 23:48:56 +0200

    feat(askllm): return response from AI

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,4 +1,5 @@
 import { AskLLM } from "components/AskLLM";
+import { CopyText } from "components/CopyText";
 
 export default async function Page() {
   return (
@@ -7,6 +8,7 @@ export default async function Page() {
         predefinedPrompt="Generate a comma-separated list of 10 ice cream flavours:"
         predefinedOutput="Chocolate, Vanilla, Strawberry, Mint Chip, Rocky Road, Cookie Dough, Butter Pecan, Neapolitan, Coffee, Coconut"
       />
+      <CopyText content="Testing content to copy" />
     </>
   );
 }
--- components/AskLLM.tsx
+++ components/AskLLM.tsx
@@ -2,40 +2,65 @@
 import { Textarea } from "@shadcn/ui/textarea";
 import { Button } from "@shadcn/ui/button";
 import { Separator } from "@shadcn/ui/separator";
-import { askLlm } from "app/[...page]/actions";
 import { Typography } from "./Typography";
-import { experimental_useFormState as useFormState } from "react-dom";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
-
-const initialState = {
-  output: null,
-};
+import { useChat } from "ai/react";
+import { FormEvent } from "react";
 
 interface AskLLMProps {
   predefinedPrompt?: string;
   predefinedOutput?: string;
+  apiKey?: string;
+}
+
+function getApiKey() {
+  const localApiKey = localStorage.getItem("apiKey");
+  if (localApiKey) {
+    return localApiKey;
+  }
+
+  const newApiKey = prompt("Please enter apiKey for service");
+  localStorage.setItem("apiKey", newApiKey);
+  return newApiKey;
 }
 
 export function AskLLM({
   predefinedPrompt = "",
   predefinedOutput = "",
 }: AskLLMProps) {
-  const [state, formAction] = useFormState(askLlm, initialState);
-  const { pending } = useFormStatus();
+  const { messages, isLoading, reload } = useChat({
+    api: "/api/chat",
+    initialInput: predefinedPrompt,
+    initialMessages: [
+      { id: "0", role: "assistant", content: predefinedOutput },
+    ],
+  });
+
+  const handleForm: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const input = formData.get("prompt");
+    const apiKey = getApiKey();
+    const inputMessages: Omit<(typeof messages)[0], "id">[] = [
+      { role: "user", content: input as string },
+    ];
+    reload({
+      options: { body: { apiKey, messages: inputMessages } },
+    });
+  };
 
   return (
     <div className="flex flex-col md:flex-row gap-4">
       <div className="flex flex-col gap-2 w-full md:w-1/2">
         <Typography variant="text">Prompt</Typography>
-        <form className="contents" action={formAction}>
+        <form className="contents" onSubmit={handleForm}>
           <Textarea
             name="prompt"
             required
             placeholder="Write your prompt here"
-            className="border-2 border-gray-300 p-2 rounded-md w-full"
+            className="border-2 border-gray-300 p-2 rounded-md w-full h-28"
             defaultValue={predefinedPrompt}
           ></Textarea>
-          <Button type="submit" disabled={pending}>
+          <Button type="submit" disabled={isLoading}>
             Generate
           </Button>
         </form>
@@ -43,7 +68,9 @@ export function AskLLM({
       <Separator orientation="vertical" className="h-auto hidden md:block" />
       <div className="flex flex-col gap-2 w-full md:w-1/2">
         <Typography variant="text">Output</Typography>
-        <div className="bg-green-300">{state.output ?? predefinedOutput}</div>
+        <div className="max-h-40 overflow-auto p-1">
+          {messages.filter((m) => m.role === "assistant").at(-1)?.content}
+        </div>
       </div>
     </div>
   );
