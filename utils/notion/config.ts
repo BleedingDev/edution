a986e2f9e03f3f8b946c706d4caba06d6012ef0f
Emmanuel odii <emmanuelodii80@gmail.com>
2023-10-20 07:24:09 +0100

    feat: add initial notion boilerplate

---
+++ utils/notion/config.ts
@@ -0,0 +1,6 @@
+// TODO: change this to the notion ID of the page you want test.
+// TODO: This should be dynamic based on user's search
+export const rootNotionPageId = "60801a81592e4292ab2a0190e7e3c29c";
+
+export const isDev =
+  process.env.NODE_ENV === "development" || !process.env.NODE_ENV;
