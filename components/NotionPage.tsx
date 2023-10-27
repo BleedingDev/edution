70699b3e28a2e973248c9b5ee6325303ce9c4113
Petr Glaser <syreanis+1@gmail.com>
2023-10-28 01:01:11 +0200

    fix(notion): fix link from the main page

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -14,6 +14,7 @@ const linkFactory =
   (nested: string = "") =>
   // eslint-disable-next-line react/display-name
   (props: PropsFrom<typeof Link>) => <Link {...props} href={`${nested}${props.href}`} />
+
 // -----------------------------------------------------------------------------
 // dynamic imports for optional components
 // -----------------------------------------------------------------------------
