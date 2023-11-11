62050f6903af67bf3d296c12824fdffca3447a73
Emmanuel odii <emmanuelodii80@gmail.com>
2023-11-11 13:05:37 +0100

    feat(editor): allow modal edits for text editor

--- components/CustomInputs/TextEditorInput.tsx
+++ components/CustomInputs/TextEditorInput.tsx
@@ -1,18 +1,29 @@
 "use client"
 
+import { useState } from "react"
+import { Dialog, DialogContent } from "@shadcn/ui/dialog"
 import { Editor } from "novel"
 
 export function TextEditorInput({ value, onChange }) {
+  const [openModal, setOpenModal] = useState(false)
+
   return (
-    <div className='flex flex-col gap-4'>
-      <Editor
-        defaultValue={value.raw}
-        completionApi='/'
-        onDebouncedUpdate={(editor) => {
-          onChange({ raw: editor?.storage.markdown.getMarkdown(), html: editor?.getHTML() })
-        }}
-        disableLocalStorage
-      />
+    <div className='rounded-lg border py-2 text-center'>
+      <button onClick={() => setOpenModal(() => true)} className='w-full'>
+        Open Editor
+      </button>
+      <Dialog open={openModal} onOpenChange={() => setOpenModal(false)}>
+        <DialogContent className='w-full'>
+          <Editor
+            defaultValue={value.raw}
+            completionApi='/'
+            onDebouncedUpdate={(editor) => {
+              onChange({ raw: editor?.storage.markdown.getMarkdown(), html: editor?.getHTML() })
+            }}
+            disableLocalStorage
+          />
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
