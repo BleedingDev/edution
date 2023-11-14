7a643a7953d3741322a1748f691918f1a1f0d5a6
Petr Glaser <syreanis+1@gmail.com>
2023-11-14 01:05:29 +0100

    style(prettier): reformat with prettier

--- components/Embed.tsx
+++ components/Embed.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React from "react"
 
 interface Props {
-  src: string;
-  title?: string;
-  width?: number;
-  height?: number;
+  src: string
+  title?: string
+  width?: number
+  height?: number
 }
 
 export function Embed({ src, title, width, height }: Props) {
   return (
     <iframe
-      referrerPolicy="no-referrer"
-      sandbox="allow-forms allow-scripts"
+      referrerPolicy='no-referrer'
+      sandbox='allow-forms allow-scripts'
       src={src}
       title={title}
       width={`${width ?? 300}px`}
       height={`${height ?? 100}px`}
-      frameBorder="0"
+      frameBorder='0'
       allowFullScreen
     ></iframe>
-  );
+  )
 }
